3c7840acf04c1151223aa481a8c0721b719a6e7e
Andreas Hjortland <git@hjortland.org>
2024-10-12 19:59:39 +0200

    Angular 16

--- src/app/client/client-resolver.service.ts
+++ src/app/client/client-resolver.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  Resolve,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ClientCommunicationService } from './client-communication.service';
 
 @Injectable()
-export class ClientResolverService implements Resolve<void> {
+export class ClientResolverService  {
   constructor(public readonly service: ClientCommunicationService) {}
 
   resolve(
